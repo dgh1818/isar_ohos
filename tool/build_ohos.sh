1b0ee08f28c05244190b90da3b4193b60d3f3a73
liujiake <liujiake999@163.com>
2024-09-07 15:39:52 +0800

    添加ohos平台的libisar.so编译脚本
    Signed-off-by: liujiake <liujiake999@163.com>

---
+++ tool/build_ohos.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    export NDK_HOST_TAG="darwin-x86_64"
+elif [[ "$(uname -s)" == "Linux" ]]; then
+    export NDK_HOST_TAG="linux-x86_64"
+else
+    echo "Unsupported OS."
+    exit
+fi
+
+#NDK=${ANDROID_NDK_HOME:-${ANDROID_NDK_ROOT:-"$ANDROID_SDK_ROOT/ndk"}}
+#COMPILER_DIR="$NDK/toolchains/llvm/prebuilt/$NDK_HOST_TAG/bin"
+#export PATH="$COMPILER_DIR:$PATH"
+
+COMPILER_DIR="/home/liujk/tools/command-line-tools/sdk/HarmonyOS-NEXT-DB5/openharmony/native/llvm/bin"
+export PATH="$COMPILER_DIR:$PATH"
+
+
+echo "$COMPILER_DIR"
+
+
+export CC_aarch64_unknown_linux_ohos=$COMPILER_DIR/aarch64-unknown-linux-ohos-clang
+export AR_aarch64_unknown_linux_ohos=$COMPILER_DIR/llvm-ar
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_OHOS_LINKER=$COMPILER_DIR/aarch64-unknown-linux-ohos-clang
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_OHOS_AR=$COMPILER_DIR/llvm-ar
+
+rustup target add aarch64-unknown-linux-ohos
+cargo build --target aarch64-unknown-linux-ohos --features sqlcipher-vendored --release
+mv "target/aarch64-unknown-linux-ohos/release/libisar.so" "libisar_ohos_arm64.so"
