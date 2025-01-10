b39526067aa649926b3f23a1bdeda3e791fe7e4a
dgh18 <dgh1218@outlook.com>
2025-01-10 12:27:41 +0800

    Changes to be committed: 	ohos

--- packages/isar_test/lib/src/init_native.dart
+++ packages/isar_test/lib/src/init_native.dart
@@ -5,55 +5,23 @@ import 'package:isar/isar.dart';
 import 'package:path/path.dart' as path;
 
 Future<void> init() async {
-  if (Platform.isMacOS || Platform.isLinux || Platform.isWindows) {
-    final rootDir = path.dirname(path.dirname(Directory.current.path));
-    final binaryName = Platform.isWindows
-        ? 'isar.dll'
-        : Platform.isMacOS
-            ? 'libisar.dylib'
-            : 'libisar.so';
+  
+   final rootDir = path.dirname(path.dirname(Directory.current.path));
+   final binaryName = 'libisar.so'
+        
     try {
       await Isar.initializeIsarCore();
     } catch (e) {
       await Isar.initializeIsarCore(
         libraries: {
-          Abi.macosArm64: path.join(
+          path.join(
             rootDir,
             'target',
-            'aarch64-apple-darwin',
-            'release',
-            binaryName,
-          ),
-          Abi.macosX64: path.join(
-            rootDir,
-            'target',
-            'x86_64-apple-darwin',
-            'release',
-            binaryName,
-          ),
-          Abi.linuxArm64: path.join(
-            rootDir,
-            'target',
-            'aarch64-unknown-linux-gnu',
-            'release',
-            binaryName,
-          ),
-          Abi.linuxX64: path.join(
-            rootDir,
-            'target',
-            'x86_64-unknown-linux-gnu',
-            'release',
-            binaryName,
-          ),
-          Abi.windowsX64: path.join(
-            rootDir,
-            'target',
-            'x86_64-pc-windows-msvc',
+            'aarch64-unknown-linux-ohos',
             'release',
             binaryName,
           ),
         },
       );
     }
-  }
 }
